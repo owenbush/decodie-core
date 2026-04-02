399419ab0abd910e0f4a32557ac9a7979d05791e
Owen Bush <owen.bush@lullabot.com>
2026-04-02 08:55:01 -0600

    Add .gitignore and remove node_modules and dist from tracking

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './types';
-export * from './parser';
-export * from './reference-resolver';
-//# sourceMappingURL=index.d.ts.map
--- dist/types.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// Types matching the JSON schemas in schema/
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=types.js.map
